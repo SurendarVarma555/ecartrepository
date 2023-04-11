7499ce8d2b8923511ca0960d25fc26c0822a7346
Surendar <batthulasurendar@gmail.com>
2023-04-11 17:25:23 +0530

    Calling doPayment API from order-service using FeignClient approach

--- order-service/src/main/java/com/dailycodebuffer/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/dailycodebuffer/service/impl/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dailycodebuffer.service.impl;
 
 import com.dailycodebuffer.entity.Order;
+import com.dailycodebuffer.external.client.PayementService;
 import com.dailycodebuffer.external.client.ProductService;
+import com.dailycodebuffer.external.client.request.PaymentRequest;
 import com.dailycodebuffer.model.OrderRequest;
 import com.dailycodebuffer.repository.OrderRepository;
 import com.dailycodebuffer.service.OrderService;
@@ -20,6 +22,9 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private PayementService payementService;
 
     @Override
     public long placeOrder(OrderRequest orderRequest){
@@ -42,7 +47,26 @@ public class OrderServiceImpl implements OrderService{
                 .build();
 
         order = orderRepository.save(order);
+        log.info("Calling Payment service to complete the payment");
 
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                                        .orderId(order.getId())
+                                        .paymentMode(orderRequest.getPaymentMode())
+                                        .amount(orderRequest.getAmount())
+                                        .build();
+        String orderStatus = null;
+        try {
+            payementService.doPayment(paymentRequest);
+            log.info("Payment done successfully and Order status changing to PLACED");
+            orderStatus = "PLACED";
+        } catch (Exception e){
+            log.info("Error occured in Payment and Order status changing to PAYMENT_FAILED");
+            orderStatus = "PAYMENT_FAILED";
+        }
+        order.setOrderStatus(orderStatus);
+        orderRepository.save(order);
+
+        log.info("Order placed successfully with Order Id: {}", order.getId());
         return order.getId();
     }
 }
